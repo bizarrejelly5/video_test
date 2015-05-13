89e2363efce0b677c49f1163fe599e7a6ec28a06
chris <chris@something.com>
2015-05-12 21:44:44 -0700

    added buttons and content, and changed sidebar

--- js/main.js
+++ js/main.js
@@ -3,11 +3,17 @@ function serviceClicked(x){
 		if(i == x){
 			document.getElementById("servicePic" + i + "").style.display = "block";
 			document.getElementById("serviceBar").style.opacity = "0.8";
+			document.getElementById("service" + i + "").style.backgroundColor = "#0B2343";
+			document.getElementById("service" + i + "").style.color = "white";
 		}
 		else{
 			document.getElementById("servicePic" + i + "").style.display = "none";
+			document.getElementById("service" + i + "").style.backgroundColor = "transparent";
+			document.getElementById("service" + i + "").style.color = "black";
 		}
 		document.getElementById("home").style.display = "none";
+		document.getElementById("homeText").style.display = "none";
+
 	}
 }
 
@@ -24,8 +30,8 @@ function serviceClicked(x){
 	player = new YT.Player('home', {
 		height: "100%",
 		width: '100%',
-		videoId: 'B8TGkhmQRko',
-		playerVars: {'controls': 0, 'disablekb': 1, 'iv_load_policy': 3, 'loop': 1, 'showinfo': 0, "playlist": 'B8TGkhmQRko'},
+		videoId: 'eIzCAA98J9w',
+		playerVars: {'controls': 0, 'disablekb': 1, 'iv_load_policy': 3, 'loop': 1, 'showinfo': 0, "playlist": 'eIzCAA98J9w'},
 		events: {
 			'onReady': onPlayerReady,
 		}
