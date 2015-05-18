9610c36145a73b651684b7e29174a7cde494a22c
chris <chris@something.com>
2015-05-17 17:30:57 -0700

    added buttons to service tab

--- js/main.js
+++ js/main.js
@@ -1,20 +1,41 @@
+//the nested service pages within a category. Variables used for hiding and displaying, 1.1 and 1.2 used for first load
+var servicePage1 = 1.1;
+var servicePage2 = 1.2;
+
 function serviceClicked(x){
 	for(var i = 1; i < 6; i++){
+		// change the page to the service clicked
 		if(i == x){
 			document.getElementById("servicePic" + i + "").style.display = "block";
 			document.getElementById("serviceBar").style.opacity = "0.8";
 			document.getElementById("service" + i + "").style.backgroundColor = "#0B2343";
 			document.getElementById("service" + i + "").style.color = "white";
 		}
+		// hide the other services
 		else{
 			document.getElementById("servicePic" + i + "").style.display = "none";
 			document.getElementById("service" + i + "").style.backgroundColor = "transparent";
 			document.getElementById("service" + i + "").style.color = "black";
 		}
-		document.getElementById("home").style.display = "none";
+		// hide the video 
+		document.getElementById("video").style.display = "none";
 		document.getElementById("homeText").style.display = "none";
 
+		// Change the nav underline bar to yellow when service clicked
+		document.getElementById("services").style.borderBottom = "4px solid #ECCB5E";
+		document.getElementById("home").style.borderBottom = "0 solid #ECCB5E";
+
+		//hide the nested service pages
+		document.getElementById("service" + servicePage1 +  "").style.display = "block";
+		document.getElementById("service" + servicePage2 +  "").style.display = "none";
 	}
+}
+
+function learnMore(x, y){
+	servicePage1 = x;
+	servicePage2 = y;
+	document.getElementById("service" + x +  "").style.display = "none";
+	document.getElementById("service" + y +  "").style.display = "block";
 }
 
  var tag = document.createElement('script');
@@ -27,7 +48,7 @@ function serviceClicked(x){
       //    after the API code downloads.
       var player;
 	function onYouTubeIframeAPIReady() {
-	player = new YT.Player('home', {
+	player = new YT.Player('video', {
 		height: "100%",
 		width: '100%',
 		videoId: 'eIzCAA98J9w',
