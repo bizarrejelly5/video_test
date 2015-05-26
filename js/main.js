61719981bd64e53d51dda44f5a94fd669acfee95
chris <chris@something.com>
2015-05-25 18:24:28 -0700

    loop test

--- js/main.js
+++ js/main.js
@@ -38,28 +38,63 @@ function learnMore(x, y){
 	document.getElementById("service" + y +  "").style.display = "block";
 }
 
- var tag = document.createElement('script');
+ 	var tag = document.createElement('script');
+	tag.src = "http://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+	var player;
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video', {
+        height: '100%',
+        width: '100%',
+        videoId: 'N6Wh-2TgBqQ',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+    });
+}
+function onPlayerReady(event) {
+        event.target.playVideo();
+      }
 
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-	function onYouTubeIframeAPIReady() {
-	player = new YT.Player('video', {
-		height: "100%",
-		width: '100%',
-		videoId: 'N6Wh-2TgBqQ',
-		playerVars: {'controls': 0, 'disablekb': 1, 'iv_load_policy': 3, 'loop': 1, 'showinfo': 0, "playlist": '6OLz5qSGk8A, N6Wh-2TgBqQ'},
-		events: {
-			'onReady': onPlayerReady,
-		}
-	});
-	}
+      // 5. The API calls this function when the player's state changes.
+      //    The function indicates that when playing a video (state=1),
+      //    the player should play for six seconds and then stop.
+      var done = false;
+      function onPlayerStateChange(event) {
+        if (event.data == YT.PlayerState.ENDED) {
+          event.target.playVideo();
 
-	function onPlayerReady(event) {
-		event.target.playVideo();
-		event.target.mute();
-	}
+        }
+      }
+      function stopVideo() {
+        player.stopVideo();
+      }
+
+//     var player;
+// 	function onYouTubeIframeAPIReady() {
+// 		player = new YT.Player('video', {
+// 			height: "100%",
+// 			width: '100%',
+// 			videoId: 'N6Wh-2TgBqQ',
+// 		// 	// playerVars: {'controls': 0, 'disablekb': 1, 'iv_load_policy': 3, 'loop': 1, 'showinfo': 0, "playlist": '6OLz5qSGk8A, N6Wh-2TgBqQ'},
+// 			playerVars: {'controls': 0, 'disablekb': 1, 'iv_load_policy': 3, 'loop': 1, 'showinfo': 0},
+// 			events: {
+// 				'onReady': onPlayerReady,
+// 				'onStateChange': onPlayerStateChange
+// 			}
+// 		});
+// 	}
+
+// 	function onPlayerReady(event) {
+// 		event.target.playVideo();
+// 		event.target.mute();
+// 	}
+// function onPlayerStateChange(event) {
+// 	if (event.data == YT.PlayerState.ENDED) {
+// 	  event.target.playVideo();
+
+// 	}
+// }
